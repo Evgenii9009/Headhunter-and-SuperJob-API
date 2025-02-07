5c2c93b11a4a5edb02fe6128a6dc66e086a5d88c
Eugene Fedorov <uptiik02@gmail.com>
2025-02-07 19:02:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ def main():
         salary, number_processed = process_vacancies_hh(vacancies)
         table_data_hh.append([language, vacancies_number,
                               number_processed, salary])
-        #vacancies, vacancies_number = get_vacancies_sj(language, date_from, secret_key)
-        #salary, number_processed = process_vacancies_sj(vacancies)
-        #table_data_sj.append([language, vacancies_number,
-        #                      number_processed, salary])
+        vacancies, vacancies_number = get_vacancies_sj(language, date_from, secret_key)
+        salary, number_processed = process_vacancies_sj(vacancies)
+        table_data_sj.append([language, vacancies_number,
+                              number_processed, salary])
 
     create_table(table_data_hh, title_hh)
-    #create_table(table_data_sj, title_sj)
+    create_table(table_data_sj, title_sj)
 
 
 if __name__ == '__main__':
